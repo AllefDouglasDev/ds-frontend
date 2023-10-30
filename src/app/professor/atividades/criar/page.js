3dd124f566febc5ea69c09edb6fe31c2b319b68b
Allef Douglas <allefwalker73@gmail.com>
2023-10-30 00:20:10 -0300

    create select and add asTextarea in Input

--- src/app/professor/atividades/criar/page.js
+++ src/app/professor/atividades/criar/page.js
@@ -1,3 +1,49 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Select } from "@/components/Select";
+import { FormProvider, useForm } from "react-hook-form";
+
 export default function CreateTaskPage() {
-  return <div>Criar atividade</div>;
+  const formMethods = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <form
+        onSubmit={formMethods.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 p-4"
+      >
+        <h1 className="text-3xl md:text-5xl font-bold">Criar atividade</h1>
+        <Input name="title" label="Título" placeholder="Digite o título" />
+        <div className="flex flex-col md:flex-row gap-4 items-center">
+          <Select
+            containerClassName="w-full"
+            name="classId"
+            label="Turma"
+            options={[{ value: 1, label: "1 ano" }]}
+          />
+          <Input
+            containerClassName="w-full"
+            name="deadline"
+            label="Prazo"
+            type="date"
+          />
+        </div>
+        <Input
+          name="description"
+          label="Descrição"
+          placeholder="Digite a descrição"
+          asTextarea
+        />
+        <Button className="mt-4" type="submit">
+          Criar atividade
+        </Button>
+      </form>
+    </FormProvider>
+  );
 }
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,24 +1,13 @@
-import { forwardRef } from "react";
 import { useFormContext } from "react-hook-form";
 
-const Component = forwardRef(({ asTextarea, ...props }, ref) => {
-  return asTextarea ? (
-    <textarea {...props} ref={ref}></textarea>
-  ) : (
-    <input {...props} ref={ref} />
-  );
-});
-
-Component.displayName = "input";
-
-export const Input = ({
+export function Select({
   containerClassName,
+  options,
   name,
   label,
   className,
-  asTextarea = false,
   ...props
-}) => {
+}) {
   const {
     register,
     formState: { errors },
@@ -27,18 +16,21 @@ export const Input = ({
   return (
     <div className={`flex flex-col gap-2 ${containerClassName}`}>
       {label && <label htmlFor={name}>{label}</label>}
-      <Component
-        asTextarea={asTextarea}
+      <select
         {...props}
         id={name}
         className={`border border-sky-700 p-2 rounded ${className}`}
         {...register(name)}
-      />
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
       {errors[name]?.message && (
         <span className="text-sm text-red-500">{errors[name].message}</span>
       )}
     </div>
   );
-};
-
-Input.displayName = "input";
+}
